3aa69c2d7e516fc6fe824367a6c092cb0966007b
mihar-22 <rahim_alwer@hotmail.com>
2019-09-03 20:10:33 +1000

    fix: incorrect bundles

--- build/build.js
+++ build/build.js
@@ -1,35 +1,51 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import alias from 'rollup-plugin-alias';
+import autoExternal from 'rollup-plugin-auto-external';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 
+import pkg from '../package.json';
 import banner from './banner';
 
-const build = (file, format) => ({
-  input: 'src/index.js',
-  output: {
-    name: 'preact-testing-library',
-    banner,
-    file,
-    format
-  },
-  plugins: [
-    alias({
-      react: 'preact/compat',
-      'react-dom': 'preact/compat'
-    }),
-    resolve(),
-    babel({
-      runtimeHelpers: true,
-      externalHelpers: true
-    }),
-    commonjs()
-  ]
+const input = 'src/index.js';
+
+const output = (file, format, globals) => ({
+  banner,
+  name: pkg.name,
+  file,
+  format,
+  globals: { preact: 'preact', ...globals }
 });
 
+const plugins = [
+  alias({
+    react: 'preact/compat',
+    'react-dom': 'preact/compat'
+  }),
+  resolve(),
+  babel({
+    runtimeHelpers: true,
+    externalHelpers: true
+  }),
+  commonjs({ include: 'node_modules/**' })
+];
+
 export default [
-  build('dist/index.js', 'umd'),
-  build('dist/preact-testing-library/preact.esm.js', 'esm')
+  {
+    input,
+    output: output('dist/pure.umd.js', 'umd'),
+    plugins,
+    external: ['preact']
+  },
+  {
+    input,
+    output: [
+      output(pkg.main, 'es'),
+      output(pkg.module, 'esm'),
+      output('dist/pure.cjs.js', 'cjs')
+    ],
+    plugins: plugins.concat([autoExternal()])
+  }
 ];
